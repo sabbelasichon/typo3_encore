ebbd978267aae9f499052fe5681f689d07955d4c
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-04-10 12:12:34 +0200

    [TASK] Add copyright comment

--- Classes/Aspect/ResourceFactorySlot.php
+++ Classes/Aspect/ResourceFactorySlot.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace Ssch\Typo3Encore\Aspect;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 
 use Ssch\Typo3Encore\Asset\JsonManifestVersionStrategy;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
--- Classes/ViewHelpers/WebpackCssFilesViewHelper.php
+++ Classes/ViewHelpers/WebpackCssFilesViewHelper.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Ssch\Typo3Encore\ViewHelpers;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use Ssch\Typo3Encore\Asset\EntrypointLookupInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
