0358f277c30e0a68d1edff20d8f2cf00ebca0e0c
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-22 18:52:16 +0100

    [TASK] Add php version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'breakpoint@schreibersebastian.de',
     'state' => 'stable',
     'clearCacheOnLoad' => false,
-    'version' => '2.0.5',
+    'version' => '2.0.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'php' => '7.2.5-7.4.999',
+            'typo3' => '9.5.0-10.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
