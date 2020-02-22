caa345a4014d02aeb6edff2b7e3ba0ae3676b919
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-22 15:32:15 +0100

    [BUGFIX] Support v10 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
