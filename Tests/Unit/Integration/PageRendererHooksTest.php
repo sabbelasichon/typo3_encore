2bcccba32b938131df2ef4cd01c7eb7c78adb1de
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-10-28 18:14:30 +0100

    [TASK] Add correct copyright
    Resolves: #55

--- Tests/Unit/Integration/PageRendererHooksTest.php
+++ Tests/Unit/Integration/PageRendererHooksTest.php
@@ -2,18 +2,7 @@
 
 namespace Ssch\Typo3Encore\Tests\Unit\Integration;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+TYPO3\TestingFramework\Fluid\Unit\ViewHelpers\ViewHelperBaseTestcase
 
 use PHPUnit\Framework\MockObject\MockObject;
 use Ssch\Typo3Encore\Asset\TagRendererInterface;
