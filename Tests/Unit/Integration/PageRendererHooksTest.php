71984954f6cad9f6bb288e1f700d0e46603a2aea
Sebastian Schreiber <me@schreibersebastian.de>
2022-12-15 21:11:57 +0100

    TASK: Make tests compatible for TYPO3 version 12

--- Tests/Unit/Integration/PageRendererHooksTest.php
+++ Tests/Unit/Integration/PageRendererHooksTest.php
@@ -34,16 +34,12 @@ final class PageRendererHooksTest extends UnitTestCase
      */
     protected $pageRenderer;
 
-    /**
-     * @var bool
-     */
-    protected $resetSingletonInstances = true;
-
     protected function setUp(): void
     {
         parent::setUp();
-        $this->tagRenderer = $this->getMockBuilder(TagRendererInterface::class)->getMock();
-        $this->pageRenderer = $this->getMockBuilder(PageRenderer::class)->getMock();
+        $this->resetSingletonInstances = true;
+        $this->tagRenderer = $this->createStub(TagRendererInterface::class);
+        $this->pageRenderer = $this->createStub(PageRenderer::class);
         $this->subject = new PageRendererHooks($this->tagRenderer);
     }
 
