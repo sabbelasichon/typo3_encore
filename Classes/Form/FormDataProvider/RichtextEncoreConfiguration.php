84a5750fe036b53bcfedb782842bd3047846da25
Timo Webler <timo.webler@dkd.de>
2023-03-27 12:16:16 +0200

    TASK: Fix contentCSS option for TYPO3 12 (#186)

--- Classes/Form/FormDataProvider/RichtextEncoreConfiguration.php
+++ Classes/Form/FormDataProvider/RichtextEncoreConfiguration.php
@@ -15,17 +15,24 @@ use Ssch\Typo3Encore\Asset\EntrypointLookupCollection;
 use Ssch\Typo3Encore\Asset\EntrypointLookupCollectionInterface;
 use Ssch\Typo3Encore\Asset\EntrypointLookupInterface;
 use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class RichtextEncoreConfiguration implements FormDataProviderInterface
 {
     private EntrypointLookupCollectionInterface $entrypointLookupCollection;
 
-    public function __construct(EntrypointLookupCollectionInterface $entrypointLookupCollection = null)
-    {
+    private int $majorVersion = 12;
+
+    public function __construct(
+        EntrypointLookupCollectionInterface $entrypointLookupCollection = null,
+        Typo3Version $typo3Version = null
+    ) {
         $this->entrypointLookupCollection = $entrypointLookupCollection ?? GeneralUtility::makeInstance(
             EntrypointLookupCollection::class
         );
+        $typo3Version = $typo3Version ?? GeneralUtility::makeInstance(Typo3Version::class);
+        $this->majorVersion = $typo3Version->getMajorVersion();
     }
 
     public function addData(array $result): array
@@ -47,39 +54,52 @@ final class RichtextEncoreConfiguration implements FormDataProviderInterface
             }
             $contentsCss = $rteConfiguration['editor']['config']['contentsCss'];
 
-            // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-contentsCss
-            // contentCss could be an array or string. We only work with arrays
-            $contentsCss = (array) $contentsCss;
-
-            $updatedContentCss = [];
-            foreach ($contentsCss as $cssFile) {
-                if (! str_starts_with($cssFile, 'typo3_encore:')) {
-                    // keep the css file as-is
-                    $updatedContentCss[] = $cssFile;
-                    continue;
+            if (is_array($contentsCss) && $this->majorVersion < 12) {
+                $updatedContentCss = [];
+                foreach ($contentsCss as $cssFile) {
+                    $updatedContent = $this->getContentCss($cssFile);
+                    $updatedContentCss[] = is_array($updatedContent) ? $updatedContent : [$updatedContent];
                 }
-
-                // strip prefix
-                $cssFile = substr($cssFile, strlen('typo3_encore:'));
-                $buildAndEntryName = GeneralUtility::trimExplode(':', $cssFile, true, 2);
-                $buildName = EntrypointLookupInterface::DEFAULT_BUILD;
-
-                if (2 === count($buildAndEntryName)) {
-                    [$buildName, $entryName] = $buildAndEntryName;
-                } else {
-                    $entryName = $buildAndEntryName[0];
-                }
-
-                $entryPointLookup = $this->entrypointLookupCollection->getEntrypointLookup($buildName);
-                $cssFiles = $entryPointLookup->getCssFiles($entryName);
-                $updatedContentCss = array_merge($updatedContentCss, $cssFiles);
-                // call reset() to allow multiple RTEs on the same page.
-                // Otherwise only the first RTE will have the CSS.
-                $entryPointLookup->reset();
+                $contentsCss = array_merge(...$updatedContentCss);
+            } elseif (is_string($contentsCss)) {
+                $contentsCss = $this->getContentCss($contentsCss);
             }
-            $result['processedTca']['columns'][$fieldName]['config']['richtextConfiguration']['editor']['config']['contentsCss'] = $updatedContentCss;
+            $result['processedTca']['columns'][$fieldName]['config']['richtextConfiguration']['editor']['config']['contentsCss'] = $contentsCss;
         }
 
         return $result;
+    }
+
+    /**
+     * @return array|string
+     */
+    private function getContentCss(string $contentsCss)
+    {
+        if (! str_starts_with($contentsCss, 'typo3_encore:')) {
+            // keep the css file as-is
+            return ($this->majorVersion < 12) ? [$contentsCss] : $contentsCss;
+        }
+
+        // strip prefix
+        $cssFile = substr($contentsCss, strlen('typo3_encore:'));
+        $buildAndEntryName = GeneralUtility::trimExplode(':', $cssFile, true, 2);
+        $buildName = EntrypointLookupInterface::DEFAULT_BUILD;
+
+        if (2 === count($buildAndEntryName)) {
+            [$buildName, $entryName] = $buildAndEntryName;
+        } else {
+            $entryName = $buildAndEntryName[0];
+        }
+
+        $entryPointLookup = $this->entrypointLookupCollection->getEntrypointLookup($buildName);
+        $cssFiles = $entryPointLookup->getCssFiles($entryName);
+        // call reset() to allow multiple RTEs on the same page.
+        // Otherwise only the first RTE will have the CSS.
+        $entryPointLookup->reset();
+        if ($this->majorVersion < 12) {
+            return $cssFiles;
+        }
+        // TYPO3 12 support only one css file. See https://forge.typo3.org/issues/99327
+        return reset($cssFiles);
     }
 }
