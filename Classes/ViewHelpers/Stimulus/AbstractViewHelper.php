a62ed477771440295cf664d2c254ce521780c4f9
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2023-04-01 21:46:42 +0200

    Update Classes/ViewHelpers/Stimulus/AbstractViewHelper.php

--- Classes/ViewHelpers/Stimulus/AbstractViewHelper.php
+++ Classes/ViewHelpers/Stimulus/AbstractViewHelper.php
@@ -151,7 +151,8 @@ abstract class AbstractViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\Abst
             trigger_deprecation(
                 'symfony/webpack-encore-bundle',
                 'v1.15.0',
-                'Passing an array as first argument of stimulus_target() is deprecated.'
+                'Passing an array as first argument of stimulus_target() is deprecated.',
+                E_USER_DEPRECATED
             );
 
             if (null !== $targetNames) {
